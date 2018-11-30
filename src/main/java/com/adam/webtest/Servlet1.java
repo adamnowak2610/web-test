2478d587e3dade9ba74eb1ecbc9d2be0638ee6ec
Adam Nowak <adasio26.10@gmail.com>
2018-12-01 00:35:48 +0100

    first servlet and jsp

--- src/main/java/com/adam/webtest/Servlet1.java
+++ src/main/java/com/adam/webtest/Servlet1.java
@@ -1,4 +1,28 @@
 package com.adam.webtest;
 
-public class Servlet1 {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "Servlet1"
+        , urlPatterns = { "/test" }
+)
+public class Servlet1 extends HttpServlet {
+
+    public void service(HttpServletRequest req, HttpServletResponse res)
+            throws IOException {
+
+        Integer x = Integer.valueOf(req.getParameter("x"));
+        Integer y = Integer.valueOf(req.getParameter("y"));
+
+        PrintWriter out = res.getWriter();
+        out.println(x + y);
+
+    }
+
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<html>
+
+    <body>
+
+        <form action="test">
+            Liczba 1:<br>
+            <input type="text" name="x"><br>
+            Liczba 2:<br>
+            <input type="text" name="y">
+            <br/>
+            <input type="submit" value="Dodaj">
+        </form>
+
+    </body>
+
+</html>
